60340d7bb1cb8f0c25d0c83db5357372c07c68e8
David <david.bilic@gauss.hr>
2023-02-18 15:22:40 +0100

    feat(services/api/base-types/ReponseTypes): api response types

---
+++ services/api/base-types/ResponseTypes.ts
@@ -0,0 +1,55 @@
+type ProductiveError =
+  | {
+  status: 400
+  title: '*one of listed values*'
+  detail: '*...* is not supported on this endpoint'
+}
+  | {
+  status: 401
+  title: 'Unauthenticated'
+  detail: 'You are not authenticated'
+}
+  | {
+  status: 403
+  title: 'Access Denied'
+  detail: 'You are not authorized to access this resource'
+}
+  | {
+  status: 404
+  title: 'Record Not Found'
+  detail: 'The requested record was not found'
+}
+  | {
+  status: 406
+  title: 'Not Acceptable'
+  detail: 'The request was not accepted'
+}
+  | {
+  status: 415
+  title: 'Unsupported Media Type'
+  detail: 'Unsupported content type'
+}
+  | {
+  status: 422
+  title: 'Invalid Attribute'
+  detail: 'Unsupported content type'
+}
+  | {
+  status: 500
+  title: 'Server error'
+  detail: 'An error occured on the server'
+}
+
+interface SuccessResponse<T> {
+  data: T
+  errored: false
+}
+
+interface ErrorResponse {
+  errors: ProductiveError[]
+  errored: true
+}
+
+type ResponseOrError<T> = SuccessResponse<T> | ErrorResponse
+
+export type ProductiveApiPromise<T = unknown> = Promise<ResponseOrError<T>>
