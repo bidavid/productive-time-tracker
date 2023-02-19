d238ca89db755715460a832bbccd55ccfd02c0e2
David <david.bilic@gauss.hr>
2023-02-19 22:05:40 +0100

    feat(form/BaseInputWrapper): done

---
+++ base-components/form/BaseInputWrapper.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="text-body-2 text-gray-600">
+    <label v-if="label" :for="inputId" class="block mb-1">{{ label }}</label>
+    <div>
+      <slot />
+    </div>
+    <p
+      v-show="charMaxLength || charMaxLength == 0"
+      class="font-medium text-gray-800 text-caption text-right mt-1"
+    >
+      {{ charMaxLength }}
+    </p>
+    <strong v-show="errorMessage" class="block font-normal mt-1">{{
+      errorMessage
+    }}</strong>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    label: {
+      type: String,
+      default: null
+    },
+    errorMessage: {
+      type: String,
+      default: null
+    },
+    charMaxLength: {
+      type: [Number, String],
+      default: null
+    },
+    inputId: {
+      type: String,
+      default: null
+    }
+  }
+})
+</script>
