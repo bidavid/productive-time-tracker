49ac11b13335a06aef5738fcb2bb83acda2de27e
David <david.bilic@gauss.hr>
2023-02-19 06:18:16 +0100

    feat(utilities/array-manipulation): new utility functions

---
+++ utility-functions/array-manipulation.ts
@@ -0,0 +1,18 @@
+/**
+ Replaces array elements *in-place*
+ @param target target array
+ @param items new items to place into target array
+ */
+function replaceItems<T extends unknown[]> (target: T, items: T) {
+  target.splice(0, target.length, ...items)
+}
+
+/**
+ Empties an array
+ @param target target array
+ */
+function clearItems<T extends unknown[]> (target: T) {
+  target.splice(0, target.length)
+}
+
+export { replaceItems, clearItems }
