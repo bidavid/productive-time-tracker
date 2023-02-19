5cf7947123375fa33a1187da6a0e6590d137af31
David <david.bilic@gauss.hr>
2023-02-19 16:30:43 +0100

    feat(BaseSpinner): done

---
+++ base-components/BaseSpinner.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :style="computedStyle" class="rounded-full animate-spin" />
+</template>
+<script lang="ts">
+import Vue from 'vue/types/index'
+
+export default Vue.extend({
+  props: {
+    size: {
+      type: String,
+      default: '1rem'
+    },
+    borderWidth: {
+      type: String,
+      default: '2px'
+    },
+    pathColor: {
+      type: String,
+      default: '#f0f2f4'
+    },
+    spinnerColor: {
+      type: String,
+      default: '#324076'
+    }
+  },
+
+  computed: {
+    computedStyle() {
+      return `
+        height: ${this.size};
+        width:  ${this.size};
+        border-width: ${this.borderWidth};
+        border-color: ${this.pathColor};
+        border-top-color: ${this.spinnerColor};
+      `
+    }
+  }
+})
+</script>
