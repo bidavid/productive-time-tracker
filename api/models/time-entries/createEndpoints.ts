edc9c058de0f41fc05cf754ee0d71d5a796f4e31
David <david.bilic@gauss.hr>
2023-02-21 12:49:13 +0100

    chore(api/time-entries/createEndpoints): typing fixed

--- api/models/time-entries/createEndpoints.ts
+++ api/models/time-entries/createEndpoints.ts
@@ -1,11 +1,11 @@
 // Types
 import type { NuxtAxiosInstance } from '@nuxtjs/axios'
 import { PaginatedResponse, PaginationFilters } from '../../types/Pagination'
-import { ModelEndpoints } from '../../types/ModelEndpoints'
 import { TimeEntry, TimeEntryFilters } from './TimeEntry'
+import { TimeEntryModelEndpoints } from '~/api/models/time-entries/TimeEntryModelEndpoints'
 
 // Every endpoint factory has to return ModelEndpoints type
-export default (axios: NuxtAxiosInstance): ModelEndpoints<TimeEntry> => ({
+export default (axios: NuxtAxiosInstance): TimeEntryModelEndpoints => ({
   // Return types can now be omitted but let's keep them
   getList(
     params: Partial<PaginationFilters & TimeEntryFilters> = {}
