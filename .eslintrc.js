62ecaff1c979a6ae1ad1635dede81afbd84c63c1
David <david.bilic@gauss.hr>
2023-03-01 17:39:38 +0100

    fix(.eslintrc): few more warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,18 @@ module.exports = {
   rules: {
     'arrow-parens': ['error', 'always'],
     '@typescript-eslint/camelcase': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ],
     camelcase: 'off',
     'space-before-function-paren': [
       'error',
